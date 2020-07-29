ae294996a9839a739acba37e857d78098ea2b365
Zane Hitchcox <zwhitchcox@gmail.com>
2020-07-29 10:51:06 -0400

    fix buffer

---
+++ UdpSockets.js
@@ -0,0 +1,13 @@
+/**
+ * @providesModule UdpSockets
+ * @flow
+ */
+
+const UdpSocket = require('./UdpSocket')
+
+exports.createSocket = function(options) {
+  if (typeof options === 'string') options = { type: options }
+  return new UdpSocket(options)
+}
+
+exports.Socket = UdpSocket
